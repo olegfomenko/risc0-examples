6ee30341d05b9c4935b7d6f1d8177c7f420b4cf7
olegfomenko <oleg.fomenko2002@gmail.com>
2024-12-03 13:03:02 +0200

    prepare benches

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,22 +1,23 @@
 use risc0_zkvm::guest::env;
 
 use std::ops::{Add, Mul};
-use k256::{AffinePoint, Scalar, ProjectivePoint};
+use k256::Scalar;
 use k256::elliptic_curve::{Group};
 
 
 fn main() {
     // read the input
-    let (private_key, amount, g, h): (Scalar, u64, AffinePoint, AffinePoint) = env::read();
-    let commitment =
-        ProjectivePoint::from(g)
-            .mul(private_key)
-            .add(
-                ProjectivePoint::from(h).mul(Scalar::from(amount))
-            );
+    let private_key = Scalar::from(12345678909876554321u128);
+    let amount = Scalar::from(12345u64);
+
+    let g = k256::ProjectivePoint::GENERATOR;
+    // Only for educational reasons :)
+    let h = g.mul(k256::Scalar::from(12345u32));
+
+    let commitment = g.mul(private_key).add(h.mul(amount));
 
     // write public output to the journal
     env::commit(&commitment.to_affine());
-    env::commit(&g);
-    env::commit(&h);
+    env::commit(&g.to_affine());
+    env::commit(&h.to_affine());
 }
